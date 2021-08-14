dd2a55b6871a90a6a2c95ef46e3e0451706896c9
Deepak Prajapati <dp0774138@gmail.com>
2021-08-14 21:39:47 +0530

    14 commit

--- lib/Widgets/alarm_info.dart
+++ lib/Widgets/alarm_info.dart
@@ -1,29 +1,28 @@
 class AlarmInfo {
-  int id = 2;
+  int id = 1;
   late String title;
   late DateTime alarmDateTime;
   late int isPending;
   late int gradientColorIndex;
-  // late String days; //
+  late String link; //
 
   AlarmInfo({
-    id,
+    // id,
     required this.title,
     required this.alarmDateTime,
     required this.isPending,
     required this.gradientColorIndex,
-    // required this.days //
+    required this.link, //
   });
 
   factory AlarmInfo.fromMap(Map<String, dynamic> json) {
     return AlarmInfo(
-      id: json["id"],
-      title: json["title"],
-      alarmDateTime: DateTime.parse(json["alarmDateTime"]),
-      isPending: json["isPending"], // == 0 ? false : true,
-      gradientColorIndex: json["gradientColorIndex"],
-      // days: json["days"] //
-    );
+        // id: json["id"],
+        title: json["title"],
+        alarmDateTime: DateTime.parse(json["alarmDateTime"]),
+        isPending: json["isPending"], // == 0 ? false : true,
+        gradientColorIndex: json["gradientColorIndex"],
+        link: json["link"]);
   }
   Map<String, dynamic> toMap() => {
         // "id": id,
@@ -31,6 +30,6 @@ class AlarmInfo {
         "alarmDateTime": alarmDateTime.toIso8601String(),
         "isPending": isPending,
         "gradientColorIndex": gradientColorIndex,
-        // "days": days //
+        "link": link,
       };
 }
